bc29e2fde826aa82c3c0764b2434dac1b854dbd6
YeLyuUT <YeLyuUT@github.com>
2018-12-12 22:35:47 +0100

    add cityscape_db and IDA network

---
+++ imdb/semantic/cityscape_semantic_db.py
@@ -0,0 +1,105 @@
+from imdb import imdb
+import os.path as osp
+import sys
+import os
+import numpy as np
+import json
+import glob
+
+DEBUG = True
+
+class cityscape_semantic_db(imdb):
+  
+  
+  def __init__(self,cityscape_root):
+    imdb.__init__(self,'cityscape_semantic')
+    self.root = cityscape_root
+    
+  def getFineImageLabelPair(self,split):
+    splits=['train','test','val']
+    assert(split in splits)
+    label_path_template = osp.join(self.root,'gtFine',split,'*','*labelTrainIds.png')
+    label_paths_candidates = glob.glob(label_path_template)
+    image_paths = []
+    label_paths = []
+    for lbl_p in label_paths_candidates:
+      idx = lbl_p.find(self.root)
+      img_p = lbl_p[:idx]+lbl_p[idx:].replace('gtFine_labelTrainIds','leftImg8bit').replace('gtFine','leftImg8bit')
+      if osp.exists(img_p) and img_p!=lbl_p:
+        image_paths.append(img_p)
+        label_paths.append(lbl_p)
+      else:
+        raise Exception('image path:%s does not exist for label:%s'%(img_p,lbl_p))
+
+    assert(len(image_paths)==len(label_paths))
+    print('Finish preparation for image label pairs.')
+    print('Total number of image label pairs is %d'%(len(image_paths)))
+
+    return image_paths,label_paths 
+  
+  def getCoarseImageLabelPair(self,split):
+    splits=['train','train_extra','val']
+    assert(split in splits)
+    label_path_template = osp.join(self.root,'gtCoarse',split,'*','*labelTrainIds.png')
+    label_paths_candidates = glob.glob(label_path_template)
+    image_paths = []
+    label_paths = []
+    for lbl_p in label_paths_candidates:
+      idx = lbl_p.find(self.root)
+      img_p = lbl_p[:idx]+lbl_p[idx:].replace('gtCoarse_labelTrainIds','leftImg8bit').replace('gtCoarse','leftImg8bit')
+      if osp.exists(img_p) and img_p!=lbl_p:
+        image_paths.append(img_p)
+        label_paths.append(lbl_p)
+      else:
+        raise Exception('image path:%s does not exist for label:%s'%(img_p,lbl_p))
+
+    assert(len(image_paths)==len(label_paths))
+    print('Finish preparation for image label pairs.')
+    print('Total number of image label pairs is %d'%(len(image_paths)))
+
+    return image_paths,label_paths 
+      
+  def getTrainData(self,with_extra = False):
+    img_list = []
+    lbl_list = []
+    imgs,lbls = self.getFineImageLabelPair('train')
+    img_list.extend(imgs)
+    lbl_list.extend(lbls)
+    if with_extra:
+      imgs,lbls = self.getCoarseImageLabelPair('train_extra')
+      img_list.extend(imgs)
+      lbl_list.extend(lbls)
+
+    if DEBUG:
+      for i in range(10):
+        print(img_list[i])
+        print(lbl_list[i])
+      if with_extra:
+        for i in range(3010,3020):
+          print(img_list[i])
+          print(lbl_list[i])
+    return img_list,lbl_list
+
+  def getValData(self):
+    img_list = []
+    lbl_list = []
+    imgs,lbls = self.getFineImageLabelPair('val')
+    img_list.extend(imgs)
+    lbl_list.extend(lbls)
+    if DEBUG:
+      for i in range(10):
+        print(img_list[i])
+        print(lbl_list[i])
+
+    return img_list,lbl_list
+
+
+def test(root):
+  db = cityscape_semantic_db(root)
+  db.getTrainData(True)
+
+if __name__=='__main__':
+  root = '/media/yelyu/Seagate Expansion Drive/researchData/dataset/Cityscape'
+  test(root)
+    
+    
---
+++ lib_ss/IDA.py
@@ -0,0 +1,88 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn import init
+import nn as mynn
+
+DEBUG = True
+
+def IDA_ResNet50_conv5_body():
+  return IDA()
+
+class IDA_level(nn.Module):
+  """ IDA structure in one level.
+  
+  
+  """
+  def __init__(self,in_dims,out_dims):
+    super(IDA_level,self).__init__()
+    assert(len(in_dims)==len(out_dims)+1)
+    self.upsampleModules = nn.ModuleList()
+    self.lateralModules = nn.ModuleList()
+    self.relu = nn.ReLU(inplace=True)
+    for idx in range(len(in_dims)-1):      
+      #upsample to 2X size
+      i_dim = in_dims[idx+1]
+      o_dim = out_dims[idx]
+      self.upsampleModules.append(
+      nn.Sequential(
+      nn.Conv2d(i_dim,o_dim,kernel_size=1,bias=False),
+      nn.GroupNorm(num_groups=32,num_channels=o_dim),
+      nn.UpsamplingBilinear2d(scale_factor=2))
+      )
+      #lateral convolution
+      i_dim = in_dims[idx]
+      o_dim = out_dims[idx]
+      self.lateralModules.append(
+      nn.Sequential(
+      nn.Conv2d(i_dim,o_dim,kernel_size=1,bias=False),
+      nn.GroupNorm(num_groups=32,num_channels=o_dim))
+      )
+
+  def _init_weights(self):
+    def init_func(m):
+      if DEBUG:
+        print(type(m))
+      if isinstance(m, nn.Conv2d):
+        mynn.init.XavierFill(m.weight)
+        if DEBUG:
+          print('XavierFill')
+        if m.bias is not None:
+          init.constant_(m.bias, 0)
+      else:
+        if DEBUG:
+          print('No init')
+    for child_m in self.upsampleModules.children():
+      child_m.apply(init_func)
+      
+  def forward(self,*Xs):
+    assert(len(Xs)==len(in_dims))
+    
+    
+
+class IDA(nn.Module):
+  """ IDA module for semantic segmentation.
+  
+  
+  
+  """
+  def __init__(self,conv_body_func,IDA_gating = False):
+    super(IDA,self).__init__()
+    self.IDA_gating = IDA_gating
+    
+    self._init_weights()
+    self.conv_body = conv_body_func()
+    
+  
+  def _init_weights(self):
+    def init_func(m):
+      if isinstance(m,nn.Conv2d):
+        mynn.init.XavierFill(m.weight)
+        if m.bias is not None:
+            init.constant_(m.bias, 0)
+            
+    for child_m in self.children():
+      #ModuleList contains modules that are initialized by themselves
+      if not isinstance(child_m,nn.ModuleList):
+        child_m.apply(init_func)
+        
