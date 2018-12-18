c45b735c395c1b574a3f6e3709adcba9709a218e
YeLyuUT <503815404@qq.com>
2018-12-18 17:21:58 +0100

    add IDA_ss_head

--- lib_ss/config.py
+++ lib_ss/config.py
@@ -3,8 +3,8 @@ from easydict import EasyDict
 __C = EasyDict()
 cfg = __C
 
-__C.IDA.LEVEL0.out_dims = []
-__C.IDA.LEVEL1.out_dims = []
-__C.IDA.LEVEL2.out_dims = []
-__C.IDA.LEVEL3.out_dims = []
-
+__C.IDA.LEVEL0.out_dims = [256,512,1024,2048]
+__C.IDA.LEVEL1.out_dims = [32,32,32]
+__C.IDA.LEVEL2.out_dims = [32,32]
+__C.IDA.LEVEL3.out_dims = [32]
+__C.IDA.SCALES = [1./32.,1./16.,1./8.,1./4.]
---
+++ lib_ss/model/IDA_ss_head.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+import llib.initialization as init
+import lib_ss.config as config
+
+class IDA_ss_outputs(nn.Module):
+  def __init__(self, class_num):
+    self.class_num = class_num
+    self.up_sample_list = nn.ModuleList()
+    self.scales = config.IDA.SCALES
+    for scale in self.scales:
+      self.up_sample_list.append(self._upsample_layer(scale))
+    self.pred = self._prediction_layer()
+
+  def _upsample_layer(self, scale_factor):
+    return nn.UpsamplingBilinear2d(scale_factor=scale_factor)
+
+  def _prediction_layer(self):
+    i_dim = config.IDA.LEVEL0.out_dims[-1]+
+                config.IDA.LEVEL1.out_dims[-1]+
+                config.IDA.LEVEL2.out_dims[-1]+
+                config.IDA.LEVEL3.out_dims[-1]
+    o_dim = self.class_num
+    return nn.Conv2d(i_dim,o_dim,kernel_size=1,bias=False)
+
+  def forward(self, blobs):
+    assert(blobs is list)
+    assert(len(blobs)==len(self.scales))
+    same_scale_blobs = []
+    for idx in range(len(blobs)):
+      same_scale_blobs.append(self.up_sample_list(blobs[idx]))
+    cat_blob = torch.cat(same_scale_blobs,dim=1)
+    top_blob = self.pred(cat_blob)
+    return top_blob
