e613e3525234b599014a9197a0d060be8c2b2a0d
YeLyuUT <503815404@qq.com>
2018-12-18 11:02:28 +0100

    Add initialization file

---
+++ llib/initialization.py
@@ -0,0 +1,20 @@
+from lib import nn as mynn
+import torch.nn as nn
+def init_func(m):
+  if DEBUG:
+    print(m)
+  if isinstance(m, nn.Conv2d):
+    mynn.init.XavierFill(m.weight)
+    if DEBUG:
+      print('XavierFill')
+    if m.bias is not None:
+      init.constant_(m.bias, 0)
+  elif isinstance(m,nn.Sequential):
+    for mm in m.children():
+      init_func(mm)
+  elif isinstance(m,nn.ModuleList):
+    for mm in m.children():
+      init_func(mm)
+  else:
+    if DEBUG:
+      print('No init')
