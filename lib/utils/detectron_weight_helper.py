fcc7d903541829dee13b5d316c52496a5e65cc16
YeLyuUT <y.lyu@utwente.nl>
2019-01-18 11:36:34 +0100

    Modify load detectron: can load when src tensor and dst tensor not compatible

--- lib/utils/detectron_weight_helper.py
+++ lib/utils/detectron_weight_helper.py
@@ -5,23 +5,45 @@ import re
 import torch
 
 
-def load_detectron_weight(net, detectron_weight_file, force_load_all = True):
+def load_detectron_weight(net, detectron_weight_file, force_load_all = True, mapped_names_need_exist = False, raise_shape_mismatch_error=False):
+    '''
+    Args:
+    force_load_all: if True, all param names should be in the mapping list.
+    '''
     name_mapping, orphan_in_detectron = net.detectron_weight_mapping
-
+    
     with open(detectron_weight_file, 'rb') as fp:
         src_blobs = pickle.load(fp, encoding='latin1')
     if 'blobs' in src_blobs:
         src_blobs = src_blobs['blobs']
 
     params = net.state_dict()
+    not_in_mapping_list = []
+    mapped_not_in_src_blobs = []
+    src_blobs_keys = set(src_blobs.keys())
     for p_name, p_tensor in params.items():
         if not force_load_all:
             if p_name not in name_mapping:
+                not_in_mapping_list.append(p_name)                
                 continue
         d_name = name_mapping[p_name]
         if isinstance(d_name, str):  # maybe str, None or True
-            p_tensor.copy_(torch.Tensor(src_blobs[d_name]))
-
+            if not mapped_names_need_exist and d_name not in src_blobs_keys:
+                mapped_not_in_src_blobs.append('%s->%s'%(d_name, p_name))
+                continue
+            src_t = torch.Tensor(src_blobs[d_name])
+            src_blobs_keys = src_blobs_keys-set([d_name])
+            if not src_t.shape==p_tensor.shape:                
+                if raise_shape_mismatch_error:
+                    raise ValueError('detectron weight shape mis-match:%s'%(d_name))
+                else:
+                    print('detectron weight shape mis-match:%s'%(d_name))
+                    print(src_t.shape,'->',p_tensor.shape)
+            else:
+                p_tensor.copy_(src_t)
+    print('Following model weights are not in detectron mapping list:', not_in_mapping_list)
+    print('Following source blob weights are not used:', src_blobs_keys)
+    print('Following source blob weights not exist for model weights:', mapped_not_in_src_blobs)
 
 def resnet_weights_name_pattern():
     pattern = re.compile(r"conv1_w|conv1_gn_[sb]|res_conv1_.+|res\d+_\d+_.+")
