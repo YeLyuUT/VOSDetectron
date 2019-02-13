42c00c4baccf4491f3043b7e7bff0ac34feb959d
YeLyuUT <y.lyu@utwente.nl>
2019-02-13 22:34:00 +0100

    Update: modify flow align function to static method

--- lib_vos/vos_model/flow_align/functions/flow_align.py
+++ lib_vos/vos_model/flow_align/functions/flow_align.py
@@ -8,34 +8,41 @@ class FlowAlignFunction(Function):
   Args:
   spatial scale marks the difference between down sampled features and original sized flows.
   """
-  def __init__(self):
-    super(FlowAlignFunction,self).__init__()
-    self._flows = None
-    self._features = None
+  @staticmethod
+  def forward(ctx, features, flows):
+    #self._flows = torch.tensor(flows)
+    #self._features = torch.tensor(features)
+    #self.feature_size = features.size()
+    feature_size = features.size()
+    ctx.feature_size = feature_size
+    if ctx.needs_input_grad[0] or ctx.needs_input_grad[1]:
+      ctx.save_for_backward(features, flows)
     
-  def forward(self,features,flows):
-    self._flows = torch.tensor(flows)
-    self._features = torch.tensor(features)
-    self.feature_size = features.size()
-    batch_size_flo, num_channels_flo, data_height_flo, data_width_flo = self._flows.size()
+    batch_size_flo, num_channels_flo, data_height_flo, data_width_flo = flows.size()
     batch_size, num_channels, data_height, data_width = features.size()
     assert(batch_size_flo==batch_size_flo and data_height_flo==data_height and data_width_flo==data_width)
     
     output = features.new(batch_size, num_channels, data_height, data_width)
     if features.is_cuda:
-      flow_align.flow_align_forward_cuda(features, self._flows, output)
+      flow_align.flow_align_forward_cuda(features, flows, output)
     else:
       raise NotImplementedError
     return output
 
-  def backward(self, grad_output):
-    assert(self.feature_size is not None and grad_output.is_cuda)
-    batch_size, num_channels, data_height, data_width = self.feature_size
-    grad_feature = self._features.new(batch_size, num_channels, data_height,data_width).zero_()
-    #hard code the channel to 2
-    grad_flow = self._flows.new(batch_size, 2, data_height,data_width).zero_()
-    flow_align.flow_align_backward_cuda(grad_output, self._features, self._flows, grad_feature, grad_flow)
-    #self._flows = None
-    #self._features = None
+  @staticmethod
+  def backward(ctx, grad_output):
+    if ctx.needs_input_grad[0] or ctx.needs_input_grad[1]:
+      features, flows = ctx.saved_variables
+      feature_size = ctx.feature_size
+      assert(feature_size is not None and grad_output.is_cuda)
+      batch_size, num_channels, data_height, data_width = feature_size
+      grad_feature = features.new(batch_size, num_channels, data_height,data_width).zero_()
+      #hard code the channel to 2
+      grad_flow = flows.new(batch_size, 2, data_height,data_width).zero_()
+      flow_align.flow_align_backward_cuda(grad_output, features, flows, grad_feature, grad_flow)
+      #self._flows = None
+      #self._features = None
 
-    return grad_feature, grad_flow
+      return grad_feature, grad_flow
+    else:
+      return None, None
